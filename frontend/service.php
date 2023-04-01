78a609b085bfe935b4ef59764d7fa9eb9939e25d
Digitcoding <121680893+Digitcoding@users.noreply.github.com>
2023-04-01 20:27:47 +0530

    update

--- frontend/service.php
+++ frontend/service.php
@@ -55,27 +55,27 @@
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav mx-auto">
                             <li class="nav-item">
-                                <a class="nav-link active" href="/frontend/index.php">Home</a>
+                                <a class="nav-link active" href="/cpgs-main/frontend/index.php">Home</a>
                             </li>
 
                             <li class="nav-item">
-                                <a class="nav-link" href="/frontend/service.php">Services</a>
+                                <a class="nav-link" href="/cpgs-main/frontend/service.php">Services</a>
                             </li>
 
                             <li class="nav-item">
-                                <a class="nav-link" href="/frontend/faq.php">FAQs</a>
+                                <a class="nav-link" href="/cpgs-main/frontend/faq.php">FAQs</a>
                             </li>
 
                             <li class="nav-item">
-                                <a class="nav-link" href="/cpgs/index.php">Complaint Register</a>
+                                <a class="nav-link" href="/cpgs-main/cpgs/index.php">Complaint Register</a>
                             </li>
                           
                         </ul>
 
                         <div class="d-none d-lg-block">
-                            <a href="/cpgs/citizen/citizen-registration.php" class="bi-person custom-icon me-3"></a>
+                            <a href="/cpgs-main/cpgs/citizen/citizen-registration.php" class="bi-person custom-icon me-3"></a>
                             
-                                <a href="/cpgs/admin/index.php" >RC</a>
+                                <a href="/cpgs-main/cpgs/admin/index.php" >RC</a>
                         </div>
                     </div>
                 </div>
